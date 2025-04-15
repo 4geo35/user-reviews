413598e4a447f89ed2133f01154a6fff5427cf1c
Labrock <vladimirpeshkov5@yandex.ru>
2025-04-15 11:09:56 +0300

    fix route

--- src/routes/web.php
+++ src/routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 use GIS\UserReviews\Http\Controllers\Web\ReviewController;
 
 Route::middleware(["web"])
-    ->as("web.reviews")
+    ->as("web.reviews.")
     ->group(function () {
         if (! empty(config("user-reviews.webPageUrl"))) {
             $controllerClass = config("user-reviews.customWebReviewController") ?? ReviewController::class;
